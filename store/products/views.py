904f09381b10ae08f932ae4f0b81b7ecc43f8a90
Болат <sareev9797@mail.ru>
2022-09-30 23:47:41 +0300

    ""

--- store/products/views.py
+++ store/products/views.py
@@ -17,6 +17,10 @@ def test_context(request):
             {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
             {'name': 'Синяя куртка The North Face', 'price': 23725.00},
             {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+        ],
+        #'promotion': True,
+        'products_of_promotion': [
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
         ]
     }
     return render(request, 'products/test-context.html', context)
