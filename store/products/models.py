f4d1088deceb903d85ab821f4d29f9aac241148c
Болат <sareev9797@mail.ru>
2022-10-02 23:59:15 +0300

    >>>

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Категории'
+        verbose_name = "Категория"
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    description = models.TextField(blank=True)
+    short_description = models.CharField(max_length=64, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Одежды'
+        verbose_name = "Одежда"
+
+    def __str__(self):
+        return f'{self.name}|{self.category.name}'
--- store/products/views.py
+++ store/products/views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from products.models import ProductCategory, Product
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store'
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
-
-def test_context(request):
     context = {
-        'title' : 'Store',
-        'header' : 'Добро пожаловать!',
-        'username' : 'Болат Сареев',
-        'products' : [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
-            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
-        ],
-        #'promotion': True,
-        'products_of_promotion': [
-            {'name': 'Черный рюкзак Nike Heritage', 'price': 2340.00},
-        ]
+        'title': 'Store - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all(),
     }
-    return render(request, 'products/test-context.html', context)
+    return render(request, 'products/products.html', context)
+
+
